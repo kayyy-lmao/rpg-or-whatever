e1fbc1f9b1f1342d36cd1b380736e820efa77695
Kleeb Beardevil <kleebuss@gmail.com>
2024-07-20 20:06:25 -0700

    Add basic model rendering

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "raylib.h"
+#include "rpg.h"
 
 int main(void)
 {
@@ -7,18 +7,40 @@ int main(void)
 
 	InitWindow(screenWidth, screenHeight, "my rpg");
 
+	Camera camera = { 0 };
+	camera.position = (Vector3){ 10.0f, 10.0f, 10.0f };
+	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+	camera.fovy = 45.0f;
+	camera.projection = CAMERA_PERSPECTIVE;
+
+	Model model = LoadModel(MODEL_BLOCK_GREEN);
+	Texture2D texture = LoadTexture(TEXTURE_BLOCK_GREEN);
+	SetMaterialTexture(&model.materials[0], MATERIAL_MAP_DIFFUSE, texture);
+
+	Vector3 position = { 0.0f, 0.0f, 0.0f };
+
+	DisableCursor();
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
 	{
+		UpdateCamera(&camera, CAMERA_FIRST_PERSON);
+
 		BeginDrawing();	
 
 			ClearBackground(RAYWHITE);
-			DrawText("Congrats! You created your first window!", 190, 200, 20, BLACK);
+
+			BeginMode3D(camera);
+				DrawModel(model, position, 1.0f, WHITE);
+				DrawGrid(10, 1.0f);
+			EndMode3D();
 
 		EndDrawing();
 	}
 
+	UnloadTexture(texture);
+	UnloadModel(model);
 	CloseWindow();
 
 	return 0;
---
+++ src/rpg.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "raylib.h"
+
+#define ASSETS_PATH "assets/"
+#define BLOCKS_PATH "assets/models/blocks"
+
+#define MODEL_BLOCK_GREEN "assets/models/blocks/green/blockGreen.obj"
+#define TEXTURE_BLOCK_GREEN "assets/models/blocks/green/blockGreen.png"
