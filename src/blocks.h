328541864287807816ad2a6b03abde3824fa0cb0
Kleeb Beardevil <kleebuss@gmail.com>
2024-07-20 20:16:09 -0700

    Use macro concatenation

---
+++ src/blocks.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define BLOCKS_PATH MODELS_PATH "blocks/"
+
+#define MODEL_BLOCK_GREEN BLOCKS_PATH "green/blockGreen.obj"
+#define TEXTURE_BLOCK_GREEN BLOCKS_PATH "green/blockGreen.png"
--- src/rpg.h
+++ src/rpg.h
@@ -2,8 +2,4 @@
 
 #include "raylib.h"
 
-#define ASSETS_PATH "assets/"
-#define BLOCKS_PATH "assets/models/blocks"
-
-#define MODEL_BLOCK_GREEN "assets/models/blocks/green/blockGreen.obj"
-#define TEXTURE_BLOCK_GREEN "assets/models/blocks/green/blockGreen.png"
+#define MODELS_PATH "assets/models"
